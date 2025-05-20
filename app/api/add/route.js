90d0a53be4e7fe6f2503c3a9de5133841f80d4a4
Mohammed Affaq <mohammedaffaq1431@gmail.com>
2025-05-21 00:07:53 +0530

    modified:   app/api/add/route.js 	modified:   app/generate/page.js 	modified:   components/Navbar.js
    initial commit

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -7,8 +7,13 @@ export async function POST(request) {
     const db = client.db("linktree")
     const collection = db.collection("links")
 
+    const doc = await collection.findOne({handle: body.handle})
+    if(doc){
+        return Response.json({success : false, error : true, message : 'LinkNext already exist', result : null})
+    }
+
     const result = await collection.insertOne(body)
 
 
-    return Response.json({success : true, error : false, message : 'Added', result : result})
+    return Response.json({success : true, error : false, message : 'LinkNext has been created', result : result})
 }
--- app/generate/page.js
+++ app/generate/page.js
@@ -7,18 +7,34 @@ const Generate = () => {
 
     // const [link, setlink] = useState("")
     // const [linktext, setlinktext] = useState("")
-    const [links, setLinks] = useState([{link:"",linktext:""}])
+    const [links, setLinks] = useState([{ linktext: "", link: "" }])
     const [handle, sethandle] = useState("")
     const [pic, setpic] = useState("")
 
-    const addLink = async (text,link,handle) => {
+    const handleChange = (index, link, linktext) => {
+        setLinks((initialLinks) => {
+            return initialLinks.map((item, i) => {
+                if (i == index) {
+                    return { link, linktext }
+                } else {
+                    return item
+                }
+            })
+        })
+    }
+
+    const addLink = () => {
+        setLinks(links.concat([{ linktext: "", link: "" }]))
+    }
+
+    const submitLinks = async () => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
         const raw = JSON.stringify({
-            "link": link,
-            "linktext": text,
-            "handle": handle
+            "links": links,
+            "handle": handle,
+            "pic": pic
         });
 
         const requestOptions = {
@@ -30,38 +46,47 @@ const Generate = () => {
 
         const r = await fetch("http://localhost:3000/api/add", requestOptions)
         const result = await r.json()
-        toast(result.message)
-        setlink("")
-        setlinktext("")
+        if(result.success){
+            toast.success(result.message)
+            setLinks([])
+            sethandle("")
+            setpic("")
+        }
+        else {
+            toast.error(result.message)
+        }
+
     }
 
 
     return (
-        <div className='bg-[#E9C0E9] min-h-screen grid grid-cols-2'>
+        <div className='bg-[#E9C0E9] grid grid-cols-2'>
 
-            
-            <div className='col1 flex flex-col justify-end items-center'>
-                <div className='flex flex-col gap-5 my-8'>
+
+            <div className='col1 flex flex-col justify-center items-center mt-10'>
+                <div className='flex flex-col gap-4'>
                     <h1 className='text-4xl font-bold text-gray-800'>Create your LinkNext</h1>
                     <div className="item">
                         <h2 className='font-semibold text-2xl text-gray-700'>Step 1 : Claim a Handle</h2>
                         <div className='mx-4'>
-                            <input value={handle || ""} onChange={e=>{sethandle(e.target.value)}} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Choose a Handle' />
+                            <input value={handle || ""} onChange={e => { sethandle(e.target.value) }} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Choose a Handle' />
                         </div>
                     </div>
                     <div className="item">
-                        <h2 className='font-semibold text-2xl text-gray-700'>Step 2 :+ Add Links</h2>
-                        <div className='mx-4'>
-                            <input value={link || ""} onChange={e=>{setlink(e.target.value)}} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter link text' />
-                            <input value={linktext || ""} onChange={e=>{setlinktext(e.target.value)}} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter link' />
-                            <button onClick={()=>addLink(linktext,link,handle)} className='p-5 py-2 mx-2 bg-[#502274] text-white font-bold rounded-3xl'>Add Link</button>
-                        </div>
+                        <h2 className='font-semibold text-2xl text-gray-700'>Step 2 :Add Links</h2>
+                        {links && links.map((item, index) => {
+                            return <div key={index} className='mx-4'>
+                                <input value={item.link || ""} onChange={e=> { handleChange(index, e.target.value, item.linktext) }} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter link' />
+                                <input value={item.linktext || ""} onChange={e=> { handleChange(index, item.link, e.target.value) }} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter linktext' />
+                            </div>
+                        })}
+                        <button onClick={() => addLink()} className='p-5 py-2 mx-2 bg-[#502274] text-white font-bold rounded-3xl'>+ Add Link</button>
                     </div>
                     <div className="item">
                         <h2 className='font-semibold text-2xl text-gray-700'>Step 3 : Add Profile picture and Submit</h2>
                         <div className='mx-4 flex flex-col'>
-                            <input value={pic || ""} onChange={e=>{setpic(e.target.value)}} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter link to your Picture' />
-                            <button className='p-5 py-2 mx-2 my-5 w-fit bg-[#502274] text-white font-bold rounded-3xl'>Submit</button>
+                            <input value={pic || ""} onChange={e => { setpic(e.target.value) }} className='px-4 py-2 mx-2 my-2 rounded-full focus:outline-purple-400 bg-white' type="text" placeholder='Enter link to your Picture' />
+                            <button disabled={pic == "" || handle == "" || links[0].linktext == ""} onClick={() => { submitLinks() }} className='disabled:bg-gray-300 p-5 py-2 mx-2 my-5 w-fit bg-[#502274] text-white font-bold rounded-3xl'>Submit</button>
                         </div>
 
 
